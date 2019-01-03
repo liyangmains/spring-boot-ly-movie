93fc49e043cc5bfc5ce996c194551efda2c60028
liyang <1301934707.com>
2019-01-03 17:57:46 +0800

    hystrix-dashboard提交以及父项目pom修改，feign-hystrix2访问hystrix.stream页面404启动类修改

--- spring-boot-ly-movie-ribbon-feign-hystrix2/src/main/java/com/ly/MyApplication.java
+++ spring-boot-ly-movie-ribbon-feign-hystrix2/src/main/java/com/ly/MyApplication.java
@@ -3,10 +3,14 @@ package com.ly;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 import ribbon.config.RibbonConfig;
 
@@ -21,4 +25,19 @@ public class MyApplication {
 	public static void main(String[] args){
 		SpringApplication.run(MyApplication.class,args);
 	}
+	
+	/**
+	 * 页面访问http://localhost:8061/hystrix.stream出现404需要在启动类配置相关servlet
+	 * @return
+	 */
+	@Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);  //系统启动时加载顺序
+        registrationBean.addUrlMappings("/hystrix.stream");//路径
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
 }
--- spring-boot-ly-movie-ribbon-feign-hystrix2/src/main/java/com/ly/config/springConfig.java
+++ spring-boot-ly-movie-ribbon-feign-hystrix2/src/main/java/com/ly/config/springConfig.java
@@ -6,9 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.client.config.IClientConfig;
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
+
 
 @Configuration
 public class springConfig {
@@ -19,5 +17,4 @@ public class springConfig {
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
-	
 }
