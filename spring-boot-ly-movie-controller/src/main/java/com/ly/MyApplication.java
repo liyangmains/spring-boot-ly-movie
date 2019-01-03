95d00f68ab9bce7d2f02a50583a561c4c179808b
liyang <1301934707.com>
2019-01-03 12:56:23 +0800

    版本提交

--- spring-boot-ly-movie-controller/src/main/java/com/ly/MyApplication.java
+++ spring-boot-ly-movie-controller/src/main/java/com/ly/MyApplication.java
@@ -2,9 +2,7 @@ package com.ly;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
@@ -14,7 +12,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class MyApplication {
 
 	public static void main(String[] args){
-		//更新
 		SpringApplication.run(MyApplication.class, args);
 	}
 }
