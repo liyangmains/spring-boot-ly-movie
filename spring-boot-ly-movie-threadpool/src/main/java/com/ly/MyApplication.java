26c8e8e012f6e6b7d7387300916e3255d0b04b0d
liyangmains <1301934707@qq.com>
2020-04-18 21:59:57 +0800

    线程池项目创建

---
+++ spring-boot-ly-movie-threadpool/src/main/java/com/ly/MyApplication.java
@@ -0,0 +1,28 @@
+package com.ly;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(value={"com.ly.*.dao"})
+public class MyApplication {
+
+	public static void main(String[] args){
+		SpringApplication.run(MyApplication.class,args);
+	}
+}
+/*@SpringBootApplication
+@MapperScan(value = {"com.ly.*.dao"})
+public class MyApplication extends SpringBootServletInitializer{
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(MyApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
+}*/
---
+++ spring-boot-ly-movie-threadpool/src/main/java/com/ly/user/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.ly.user.controller;
+
+import com.ly.user.entity.User;
+import com.ly.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/find")
+    public User getUserList(Integer id){
+        return userService.selectUserInfoById(id);
+    }
+
+}
